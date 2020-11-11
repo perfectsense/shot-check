ff9d16fe58c05215523f11714ddfd5d764f5a380
Lee Teague <lee@perfectsensedigital.com>
2020-11-11 12:06:52 -0500

    Fix the scroll height bug in some cases

--- src/main/comparisonJob.js
+++ src/main/comparisonJob.js
@@ -61,7 +61,11 @@ async function autoScroll(page, turbo, description) {
         try {
           var totalHeight = 0
           var timer = setInterval(async () => {
-            var scrollHeight = document.body.scrollHeight
+            let scrollHeight = Math.max(
+              document.body.scrollHeight, document.documentElement.scrollHeight,
+              document.body.offsetHeight, document.documentElement.offsetHeight,
+              document.body.clientHeight, document.documentElement.clientHeight
+            )
             if (totalHeight < scrollHeight) {
               if (totalHeight % (autoScrollDistance * 7) == 0) {
                 shotCheckMessageCallback(
